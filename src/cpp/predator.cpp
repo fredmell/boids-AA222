411830761efdb8f7e6447a17282a93f002ee62a5
Frederik Mellbye <frederme@stanford.edu>
2020-05-22 11:49:18 +0200

    Add predator and prey cpp files, update stuff

---
+++ src/cpp/predator.cpp
@@ -0,0 +1,6 @@
+#include "predator.hpp"
+
+Prey(Vec2 x0, Vec2 v0)
+: Boid(x0, v0) {
+    shape.setFillColor(sf::Color::Green);};
+};
---
+++ src/cpp/prey.cpp
@@ -0,0 +1,7 @@
+#include "prey.hpp"
+#include "vec2.hpp"
+
+Prey::Prey(Vec2 x0, Vec2 v0)
+: Boid(x0, v0) {
+    shape.setFillColor(sf::Color::Green);
+}
